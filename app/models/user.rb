200c9c65b13a2c13a6daf294dfc508dfaf2862a5
Paul <paulyk1983@gmail.com>
2016-04-21 15:19:35 -0500

    added self.from_omniauth method to User model to retrieve and parse the user data sent back by omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_secret = auth.credentials.secret
+      user.save!
+    end
+  end
 end
