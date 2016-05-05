0514b000a751bccecd784b9b68042e7fef58e783
Paul <paulyk1983@gmail.com>
2016-05-04 20:05:09 -0500

    added bar chart with fake data to dashboard and corrected javascript for tweets index page

--- app/assets/javascripts/tweets_ctrl.js
+++ app/assets/javascripts/tweets_ctrl.js
@@ -6,21 +6,15 @@
   angular.module("app").controller("tweetsCtrl", function($scope, $http) {
 
     $scope.setup = function() {
-      $http.get('/api/v1/tweets.json?count=50').then(function(response) {
+      $http.get('/api/v1/tweets.json').then(function(response) {
         $scope.tweets = response.data;
       });
     };
 
     $scope.changeOrderAttribute = function(inputAttribute) {
-      if (inputAttribute !== $scope.orderAttribute) {
-        $scope.orderDescending = false;
-      } else {
-        $scope.orderDescending = !$scope.orderDescending;
-      }
       $scope.orderAttribute = inputAttribute;
     };
-
-
+    
     window.$scope = $scope;
   });
 })();
