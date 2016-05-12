eb8ca17f4426578473385b8136fe37f9ead1692f
Paul <paulyk1983@gmail.com>
2016-05-12 13:29:23 -0500

    added more javascript front end functionality to tweets/new page including adding image thumb to bottom of textarea and removing image preview when add image is clicked, and vise-versa when remove image is clicked. Also added styling to added elements

--- app/assets/javascripts/tweets_ctrl.js
+++ app/assets/javascripts/tweets_ctrl.js
@@ -24,8 +24,54 @@
         document.getElementById("word-count").style["color"] = 'green';
       }
     };
-      
+
+    $scope.changeCount = function() {
+      if (document.getElementById('image-input').value === 'true') {
+        $scope.wordCount = 116 - $scope.message.length;
+      } else {
+        $scope.wordCount = 140 - $scope.message.length;
+      }
+      if ($scope.wordCount < 0) {
+        document.getElementById("word-count").style["color"] = 'red';
+      } else {
+        document.getElementById("word-count").style["color"] = 'green';
+      }
+    };
+
+    $scope.addImage = function() {
+      document.getElementById('image-input').value = 'true';
+      $scope.wordCount = $scope.wordCount - 24;
+      var addImageButton = document.getElementById('add-image');
+      addImageButton.className += " hidden";
+
+      var removeImageButton = document.getElementById('remove-image');
+      removeImageButton.className = removeImageButton.className.replace( ' hidden', '' );
+
+      var image = document.getElementById('image');
+      image.className += "hidden";
+
+      var imageThumb = document.getElementById('image-thumb');
+      imageThumb.className = imageThumb.className.replace( 'hidden', '' );
+    };
+
+    $scope.removeImage = function() {
+      document.getElementById('image-input').value = 'false';
+      $scope.wordCount = $scope.wordCount + 24;
+      var removeImageButton = document.getElementById('remove-image');
+      removeImageButton.className += " hidden";
+
+      var addImageButton = document.getElementById('add-image');
+      addImageButton.className = removeImageButton.className.replace( ' hidden', '' );
+
+      var imageThumb = document.getElementById('image-thumb');
+      imageThumb.className += "hidden";
+
+      var image = document.getElementById('image');
+      image.className = image.className.replace( 'hidden', '' );
+    };
+
     
     window.$scope = $scope;
+  
   });
 })();
