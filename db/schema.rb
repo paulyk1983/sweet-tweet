5a86df2230e04bb05288b17303e9d2688c203fc7
Paul <paulyk1983@gmail.com>
2016-05-14 18:46:56 -0500

    added twitter_handle attribute to User mosdel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511184808) do
+ActiveRecord::Schema.define(version: 20160514234516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,8 +53,9 @@ ActiveRecord::Schema.define(version: 20160511184808) do
     t.string   "name"
     t.string   "oauth_token"
     t.string   "oauth_secret"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "twitter_handle"
   end
 
 end
