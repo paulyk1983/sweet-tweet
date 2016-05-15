c566e19a2b65e4dcc178d9f9f2dc20e7baa41c34
Paul <paulyk1983@gmail.com>
2016-05-15 17:04:18 -0500

    added background image to .wrapper section

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TweetsController < ApplicationController
   def index
     if current_user
-      options = {count: 50, include_rts: true}
+      options = {count: 100, include_rts: true}
       @tweets = current_user.client.user_timeline(current_user.twitter_handle, options)
       render 'index.json.jbuilder'     
     else
--- app/views/api/v1/tweets/index.json.jbuilder
+++ app/views/api/v1/tweets/index.json.jbuilder
@@ -1,8 +1,9 @@
 json.array! @tweets.each do |tweet|
+  json.id tweet.id
   json.text tweet.text
   json.created_at tweet.created_at
   json.retweet_count tweet.retweet_count
   json.favorites_count tweet.favorites_count
+  json.rank (tweet.retweet_count * 2) + tweet.favorites_count
   json.media tweet.media
-  json.id tweet.id
 end
