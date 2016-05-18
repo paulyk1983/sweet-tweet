1bd993bdf75ef2b0948b8b0143633b1b217984c6
Paul <paulyk1983@gmail.com>
2016-05-18 13:57:32 -0500

    added profile image to navbar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,6 +10,11 @@ class DashboardController < ApplicationController
       current_user.update(profile_pic: profile_pic)
     end
 
+    unless current_user.profile_pic
+      profile_pic = current_user.client.user.profile_image_url
+      current_user.update(profile_pic: profile_pic)
+    end
+
     @pending_pages = Page.where("status = ? AND user_id = ?", 'pending', current_user.id) 
 
     options = {count: 200, include_rts: true}
