572b6fe42049feac6e14c64a0d88a764e1312b90
Paul <paulyk1983@gmail.com>
2016-06-18 12:01:52 -0500

    created first User validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+# RSpec.describe User, type: :model do
 
-  describe 'test' do
-    it 'returns 1 if given 1' do
-      result = '1'
-      expect(result).to eq('1')
-    end
-  end 
+describe User do
+  it 'returns 1 if given 1' do
+    result = '1'
+    expect(result).to eq('1')
+  end
+
+  it 'is valid with a name, oauth_token and oauth_secret' do
+    user = User.new(
+      name: 'test',
+      oauth_token: 'test',
+      oauth_secret: 'test'
+    )
+    expect(user).to be_valid
+  end
 
   # describe 'test' do
   #   it 'should return https://finishlinecorp.com/ties2elastic/let-your-product-tags-have-a-ball 
