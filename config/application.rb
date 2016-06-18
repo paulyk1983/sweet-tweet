392370627f9b627aac04bc72c9f670e16ff0a035
Paul <paulyk1983@gmail.com>
2016-06-18 10:36:26 -0500

    reconfigured development and test environments in database.yml file to allow app to run

--- config/application.rb
+++ config/application.rb
@@ -22,17 +22,16 @@ module SweetTweet
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths += %W(#{config.root}/spec/helpers)
 
     config.generators do |g|
-      g.test_framework :rspec,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: true,
-        request_specs: false
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
+        g.test_framework :rspec,
+            fixtures: true,
+            view_specs: false,
+            helper_specs: false,
+            routing_specs: false,
+            controller_specs: true,
+            request_specs: false
+        g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
   end
 end
